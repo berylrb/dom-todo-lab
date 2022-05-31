0dc7542809dde9bc84ec54daab4ce6d73eae6ab8
Beryl B <beriebaldwin@gmail.com>
2022-05-31 17:03:18 -0400

    add list items

---
+++ js/app.js
@@ -0,0 +1,32 @@
+/*-------------------------------- Constants --------------------------------*/
+
+
+
+/*-------------------------------- Variables --------------------------------*/
+
+
+
+/*------------------------ Cached Element References ------------------------*/
+const listInput = document.querySelector("#list-input")
+const submitBtn = document.querySelector("#submit-button")
+const toDoList = document.querySelector("#todo-list")
+
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+submitBtn.addEventListener('click', newItem)
+
+
+
+/*-------------------------------- Functions --------------------------------*/
+
+function newItem(evt) {
+  const li = document.createElement('li')
+  li.textContent = listInput.value
+  toDoList.appendChild(li)
+  console.log(li)
+  listInput.value = ''
+}
+
